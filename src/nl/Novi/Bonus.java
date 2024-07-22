a7aa74426e232cc9d1de43a39c821050efd2e3ea
GvN1989 <geesje.noordenburg@gmail.com>
2024-07-22 17:41:52 +0200

    bonus uitgewerkt

--- src/nl/Novi/Bonus.java
+++ src/nl/Novi/Bonus.java
@@ -1,66 +1,76 @@
+package nl.Novi;
+
 import java.util.HashSet;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bonus {
 
     public static void main(String[] args) {
 
-        // Onderstaande code is uitgecommentarieerd. Haal de code uit de comment (delete /* en */) om aan de slag te gaan met de bonus
-        // In de code staan op verschillende plekken comments tussen "/*...*/" als hint wat je moet doen
-
-
         HashSet<Integer> secretnumber = randomnumbergenerator();
         String stringnumber = setToStringConverter(secretnumber);
         System.out.println(stringnumber);
-        feedback(/*vul hier het juiste argument in*/);
-
+        feedback(stringnumber);
     }
 
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ randomnumbergenerator(/*Heeft deze methode nog parameter(s) nodig?*/){
-         /*
-        Vul hier de body van de methode in.
-
-        Stappenplan:
-        - Maak een nieuwe variabele van type Random. (Tip: Zoek op internet hoe je Random kunt gebruiken)
-        - Maak een nieuwe variabele van type HashSet.
-        - Schrijf een while-loop om 4 random nummers aan de hashset toe te voegen
-        - return de hashset
-         */
-    }
-
-    public static void/*moet dit returntype "void" zijn of wat anders?*/ setToStringConverter(/*Heeft deze methode nog parameter(s) nodig?*/){
-        /*
-        Vul hier de body van de methode in.
-
-        Stappenplan:
-        - Maak als eerst een String variabele met als waarde een lege String. (of gebruik een StringBuilder)
-        - Schrijf vervolgens een for-loop om de items in de hashset een voor een aan de String variabele toe te voegen.
-        - Return de (gevulde) String variabele
-         */
-    }
-
-
-
-    public static void/*moet dit "void" zijn of wat anders?*/ feedback(String stringnumber) {
+    public static void feedback(String stringnumber) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder feedback = new StringBuilder();
+        boolean isCorrect = false;
+
         System.out.println("+ = juiste nummer op de juiste plek, O = juiste nummer verkeerde plek, X = verkeerde nummer");
         System.out.println("Doe een gok, Let op vul 4 getallen in.");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
-                    feedback.append("+");
-                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
-                    feedback.append("0");
-                } else {
-                    feedback.append("X");
+
+        while (!isCorrect) {
+
+            StringBuilder feedback = new StringBuilder();
+            String guess = scanner.nextLine();
+
+            if (guess.length() != 4) {
+                System.out.println("Ongeldige invoer. Zorg ervoor dat je precies 4 getallen invoert.");
+                continue;
+        }
+            if (Objects.equals(guess, stringnumber)) {
+                System.out.println("Gefeliciteerd je hebt het goed");
+                isCorrect = true;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
+                        feedback.append("+");
+                    } else if (stringnumber.contains(guess.substring(i, i + 1))) {
+                        feedback.append("O");
+                    } else {
+                        feedback.append("X");
+                    }
                 }
+                System.out.println(feedback.toString());
+                System.out.println("Probeer het opnieuw. Vul opnieuw 4 getallen in.");
             }
         }
-        System.out.println(feedback.toString());
     }
+
+    public static HashSet<Integer> randomnumbergenerator(){
+
+        Random rand = new Random();
+
+        HashSet<Integer> secretnumber = new HashSet<>();
+
+        while (secretnumber.size () <4) {
+            int randomnumber = rand.nextInt(9) +1;
+            secretnumber.add(randomnumber);}
+        return secretnumber;
+
+    }
+
+    public static String setToStringConverter(HashSet<Integer> secretnumber){
+       StringBuilder stringnumber= new StringBuilder();
+
+       for (Integer element : secretnumber) {
+           stringnumber.append(element.toString());
+       }
+       return stringnumber.toString();
+       }
+
+
 }
